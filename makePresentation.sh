068e098e74f82a1b4390b3fdde80c5a47121b459
edeno <edeno@bu.edu>
2017-01-27 11:53:20 -0500

    Add bash script to make presentation

---
+++ makePresentation.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Make presentation
+pandoc src/presentation.md \
+  --to revealjs \
+  --variable revealjs-url="revealjs" \
+  --variable slideNumber="\"c/t\"" \
+  --variable transition="none" \
+  --variable controls="false" \
+  --variable history="true" \
+  --smart \
+  --css "custom.css" \
+-o public/index.html
