6eae5a9599d5ed572cd0c114763eb20ae8ceed92
edeno <edeno@bu.edu>
2017-01-27 13:21:42 -0500

    Move revealjs code to custom folder
    Putting a forward slash in the href messes up github pages. This move
    helps the relative links reference the correct folders.

--- makePresentation.sh
+++ makePresentation.sh
@@ -2,7 +2,7 @@
 # Make presentation
 pandoc src/presentation.md \
   --to revealjs \
-  --variable revealjs-url="" \
+  --variable revealjs-url="revealjs" \
   --variable slideNumber="\"c/t\"" \
   --variable transition="none" \
   --variable controls="false" \
