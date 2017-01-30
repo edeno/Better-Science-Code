b4d27e5d3a3485489ab3e1034d1196d62f9f1a44
edeno <edeno@bu.edu>
2017-01-30 10:51:10 -0500

    Use the reveal.js highlighting

--- makePresentation.sh
+++ makePresentation.sh
@@ -2,11 +2,13 @@
 # Make presentation
 pandoc src/presentation.md \
   --to revealjs \
+  --no-highlight \
   --variable revealjs-url="revealjs" \
   --variable slideNumber="\"c/t\"" \
   --variable transition="none" \
   --variable controls="false" \
   --variable history="true" \
+  --variable hlss=zenburn \
   --smart \
   --css "css/custom.css" \
 -o index.html
