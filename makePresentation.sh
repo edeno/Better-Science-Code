7243f3883e766568a5e89b2122bdf5d80f00e9d0
edeno <edeno@bu.edu>
2017-01-30 11:02:19 -0500

    Revert "Use the reveal.js highlighting"
    This reverts commit b4d27e5d3a3485489ab3e1034d1196d62f9f1a44.

--- makePresentation.sh
+++ makePresentation.sh
@@ -2,13 +2,11 @@
 # Make presentation
 pandoc src/presentation.md \
   --to revealjs \
-  --no-highlight \
   --variable revealjs-url="revealjs" \
   --variable slideNumber="\"c/t\"" \
   --variable transition="none" \
   --variable controls="false" \
   --variable history="true" \
-  --variable hlss=zenburn \
   --smart \
   --css "css/custom.css" \
 -o index.html
